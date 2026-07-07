fd46dc7316e9c7a3289c2a7b8d66de2e0f6f5d7b
Pushpit Raj <pushpitraj19jan@gmail.com>
2026-07-07 11:50:53 +0530

    Fixes

--- backend-drf/stock_prediction/settings.py
+++ backend-drf/stock_prediction/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'stock_prediction/static',
+]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
